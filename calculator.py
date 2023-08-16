8eae8b724b73ce0d6a61a4fbbe73ab6097617bb1
ayaanmoiz <142215887+ayaanmoiz@users.noreply.github.com>
2023-08-16 10:11:02 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+
+a=int(input("enter a number "))
+b=int(input("enter a number "))
+n=int(input("choose a number from 1-4 "))
+if n==1 :
+    print(a+b)
+if n==2:
+   print(a-b)
+if n==3:
+    print(a*b)
+if n==4:
+    print(a/b)
+else:
+    print("invalid")
+
